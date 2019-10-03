2a73e61389d5785f2698e52884edbddab5e33d4c
meganpoon <meganpoonwl@gmail.com>
2019-10-03 10:15:28 +0200

    FakeRepo

--- MovieRating.Core.Entity/Rating.cs
+++ MovieRating.Core.Entity/Rating.cs
@@ -3,8 +3,15 @@ namespace MovieRating.Core.Entity
 {
     public class Rating
     {
-        public Rating()
+        public Rating() { }
+
+        public Rating(int reviewerId, int movieId, int grade, DateTime Date)
         {
+            ReviewerId = reviewerId;
+            MovieId = movieId;
+            Grade = grade;
+           Date = Date;
+
         }
 
         public int ReviewerId { get; set; }
--- MovieRating.Infrastructure.Data/Repositories/MovieRatingRepository.cs
+++ MovieRating.Infrastructure.Data/Repositories/MovieRatingRepository.cs
@@ -9,7 +9,19 @@ namespace MovieRating.Infrastructure.Data.Repositories
     { 
         public MovieRatingRepository()
         {
+            
         }
+
+        public Rating AddRating(int reviewerId, int movieId, int grade, DateTime Date)
+        {
+            FakeMovieRatingRepository fakerepo = new FakeMovieRatingRepository();
+            fakerepo.AddRating(new Rating(1, 1, 5, DateTime.Now));
+
+
+
+            return null;
+        }
+
 
         public List<Rating> GetTopRateMovies(int number)
         {
